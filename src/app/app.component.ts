2e49a76368c9f014b25be4731913d3ddbce5e4f4
Phil <philgiammattei@gmail.com>
2020-01-26 22:28:38 -0500

    enabled 1v1 logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dicematch';
+  classes = [
+    'Barbarian',
+    'Moon Elf',
+    'Monk',
+    'Pyromancer',
+    'Paladin',
+    'Shadow Thief',
+    'Samurai',
+    'Gunslinger',
+    'Cursed Pirate',
+    'Tactician',
+    'Huntress',
+    'Artificer',
+    'Vampire Lord',
+    'Seraph',
+  ];
+  player1 = '';
+  player2 = '';
+
+  matchmaker() {
+    this.player1 = this.classes[Math.floor(Math.random() * this.classes.length)];
+    this.classes = this.classes.filter(fighter => fighter !== this.player1);
+    this.player2 = this.classes[Math.floor(Math.random() * this.classes.length)];
+  }
+
+  ngOnInit() {
+    this.matchmaker();
+  }
 }
